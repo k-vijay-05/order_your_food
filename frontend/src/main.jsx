6dbc1c52245481be131637ab7dfcf08d6360edb8
vijay <kvijay20204567@gmail.com>
2024-05-12 21:44:04 +0530

    added homepage components

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import StorecontextProvider from './context/Storecontext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+        <StorecontextProvider>
+            <App />
+        </StorecontextProvider>
+
+
+
+
+
+    </BrowserRouter>
+
 )
